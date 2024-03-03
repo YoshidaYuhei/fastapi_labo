3ed6ade78d905750ea0e07c5979ac8b55af64bd7
YoshidaYuhei <yoshidayuhei@gmail.com>
2024-03-03 13:29:12 +0900

    draft:途中

--- fast/app/routers/auth.py
+++ fast/app/routers/auth.py
@@ -5,9 +5,17 @@ from fastapi import Response
 from fastapi import status
 
 from lib.auth.jwt_token import create_access_token, create_refresh_token
+from entity.user import UserSignUpIn
+from repository.user import UserRepository
+from depends.di_container import user_repository
 
 router = APIRouter()
 
-@router.post("/token")
+@router.post("/signup", response_model=None)
+async def signup(request: UserSignUpIn, user_repo: UserRepository = Depends(user_repository)):
+    user_repo.sign_up(request)
+    return True
+
+@router.post("/login")
 async def generate_token(email: Annotated[str, Body(...)]):
     return {"access_token": create_access_token(email), "refresh_token": create_refresh_token(email), "token_type": "bearer"}
--- fast/app/tests/api/test_auth.py
+++ fast/app/tests/api/test_auth.py
@@ -3,7 +3,7 @@ from fastapi import Body
 from fastapi.testclient import TestClient
 
 def test_generate_token(client: TestClient, fake: Faker):
-    response = client.post("/auth/token", json=fake.email())
+    response = client.post("/auth/login", json={ "email": fake.email(), "password": fake.password() })
     
     assert response.status_code == 200
     assert "access_token" in response.json()
